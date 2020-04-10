2b362c022f0b5cf8556d33000a00dc7469cebbdc
Matilda578 <101889190@student.swin.edu.au>
2020-04-10 17:03:04 +1000

    list created at last

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewListAttempt
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<int> biglist = new List<int>();
+                int finalnumber = 0;
+                while (biglist.Count < 5)
+                {
+                    Console.WriteLine("enter a number: " + (biglist.Count + 1));
+                    biglist.Add(Int32.Parse(Console.ReadLine()));
+                }
+                
+                Console.WriteLine("Input one more number ");
+                finalnumber = Int32.Parse(Console.ReadLine());
+
+                if (biglist.Contains(finalnumber))
+                {
+                    Console.WriteLine("this number is already entered");
+                }
+                else
+                {
+                    Console.WriteLine("this is a new numbner!");
+                }
+            }
+        }
+    }
